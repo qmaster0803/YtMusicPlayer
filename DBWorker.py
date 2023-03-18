fed1c9ca615a56fdaa76fd3d9e74fe000747bf33
q <qmaster080305@gmail.com>
2023-03-18 22:11:40 +0300

    wip

--- DBWorker.py
+++ DBWorker.py
@@ -1,5 +1,43 @@
 import sqlite3
+import os
 
 class DBWorker:
 	def __init__(self):
+		if(not os.path.exists("main.db")): db_empty = True
+		else: db_empty = False
 		self.db = sqlite3.connect("main.db")
+		if(db_empty): self.init_empty_db()
+
+	def init_empty_db(self):
+		c = self.db.cursor()
+		c.execute('CREATE TABLE "tracks" ("id" TEXT UNIQUE, "name" TEXT, "album" TEXT, "artist" TEXT, "year" INTEGER, "preview" TEXT);')
+		c.execute('CREATE TABLE "playlists" ("id" INTEGER UNIQUE, "name" INTEGER, PRIMARY KEY("id" AUTOINCREMENT));');
+		c.execute('CREATE TABLE "playlist_map" ("playlist_id" INTEGER, "track_id" INTEGER, "position" INTEGER);')
+		self.db.commit()
+
+	def save_track_data(self, track_id, name, album, artist, year, preview):
+		c = self.db.cursor()
+		c.execute('INSERT INTO `tracks`(`id`, `name`, `album`, `artist`, `year`, `preview`) VALUES ("{}", "{}", "{}", "{}", {}, "{}");'.format(str(track_id), str(name), str(album), str(artist), str(year), str(preview)))
+		self.db.commit()
+
+	def get_track_data(self, track_id):
+		c = self.db.cursor()
+		c.execute('SELECT * FROM `tracks` WHERE `id`="'+str(track_id)+'";')
+		d = c.fetchone()
+		if(d):
+			data = {}
+			data['title']        = d[1]
+			data['album']        = d[2]
+			data['artist']       = d[3]
+			data['preview']      = d[5]
+			data['release_year'] = d[4]
+			return data
+		else: return None
+
+	def get_playlists(self):
+		c = self.db.cursor()
+		c.execute('SELECT * FROM `playlists`;')
+		data = {}
+		for i in c.fetchall():
+			data[i[0]] = i[1]
+		return data
--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import DBImportWorker
+import DBWorker
 import YTWorker
 import curses
 import time
 
 class MainWorker:
-	def __init__(self, screen, yt_worker):
+	def __init__(self, screen, yt_worker, db_worker):
 		self.screen = screen
 		self.yt_worker = yt_worker
-		self.do_window_prepare()
+		self.db_worker = db_worker
 
 		#current playing data
+		self.first_displayed_playlist = 0
 		self.playback_id = 'pTYIf2pkxzQ'
 		self.timecode = None
+
+		self.do_window_prepare()
 
 	def do_window_prepare(self):
 		win_height = curses.LINES-1
 		win_width  = curses.COLS -1
 		self.global_width = int(win_width*0.3)+int(win_width*0.7)
+		self.global_lists_height = win_height-6
 
 		self.player_heading_window        = curses.newwin(1, self.global_width+1, 0, 0)
 		self.player_progressbar_window    = curses.newwin(1, self.global_width+1, 1, 0)
@@ -29,8 +34,16 @@ class MainWorker:
 		self.player_heading_window.noutrefresh()
 		self.playlists_window.noutrefresh()
 		self.tracks_window.noutrefresh()
+
 		self.update_player()
+		self.update_playlists_list()
 		curses.doupdate()
+
+	def update_playlists_list(self):
+		self.playlists = self.db_worker.get_playlists()
+		for i, playlist_id in enumerate(sorted(list(self.playlists.keys()))[self.first_displayed_playlist:self.first_displayed_playlist+self.global_lists_height]):
+			self.playlists_window.addstr(1+i, 1, "[ ] "+self.playlists[playlist_id])
+		self.playlists_window.refresh()
 
 	def update_player(self):
 		self.player_heading_window.addstr(0, 0, 'test')
@@ -44,17 +57,22 @@ class MainWorker:
 		self.player_progressbar_window.refresh()
 
 	def update_player_heading(self):
-		data = self.yt_worker.get_info(self.playback_id)
+		data = self.db_worker.get_track_data(self.playback_id)
+		#for safety
+		if(data == None):
+			data = self.yt_worker.get_info(self.playback_id)
+			self.db_worker.save_track_data(self.playback_id, data['title'], data['album'], data['artist'], data['release_year'], data['preview'])
 		s = data['title']+" - "+data['album']+" "+"["+data['artist']+']['+str(data['release_year'])+']'
 		self.player_heading_window.addstr(0, 0, s)
 		self.player_heading_window.refresh()
 
 def main(screen):
 	db_import_worker = DBImportWorker.DBImportWorker("vimusic.db")
+	db_worker = DBWorker.DBWorker()
 	yt_worker = YTWorker.YTWorker()
 	#playlists = db_import_worker.get_playlists()
 
-	main_worker = MainWorker(screen, yt_worker)
+	main_worker = MainWorker(screen, yt_worker, db_worker)
 	main_worker.update_player_heading()
 	for i in range(1000):
 		main_worker.set_player_progressbar(i)
