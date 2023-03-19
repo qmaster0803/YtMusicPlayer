73ad3496896adad30a627190af8ea8a7c1a0d734
qmaster0803 <qmaster080305@gmail.com>
2023-03-20 00:44:56 +0300

    wip

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import DBWorker
 import YTWorker
 import curses
 import time
+import os
 
 class MainWorker:
 	def __init__(self, screen, yt_worker, db_worker):
@@ -12,21 +13,27 @@ class MainWorker:
 
 		#current playing data
 		self.first_displayed_playlist = 0
+		self.selected_playlist = None
+		self.playlists_count = 0
+
 		self.playback_id = 'pTYIf2pkxzQ'
 		self.timecode = None
 
 		self.do_window_prepare()
 
+
 	def do_window_prepare(self):
 		win_height = curses.LINES-1
 		win_width  = curses.COLS -1
-		self.global_width = int(win_width*0.3)+int(win_width*0.7)
+		self.playlists_win_width = int(win_width*0.3)
+		self.tracks_win_width =    int(win_width*0.7)
+		self.global_width = self.playlists_win_width+self.tracks_win_width
 		self.global_lists_height = win_height-6
 
 		self.player_heading_window        = curses.newwin(1, self.global_width+1, 0, 0)
 		self.player_progressbar_window    = curses.newwin(1, self.global_width+1, 1, 0)
-		self.playlists_window             = curses.newwin(win_height-4, int(win_width*0.3), 2, 0)
-		self.tracks_window                = curses.newwin(win_height-4, int(win_width*0.7), 2, int(win_width*0.3))
+		self.playlists_window             = curses.newwin(win_height-4, self.playlists_win_width, 2, 0)
+		self.tracks_window                = curses.newwin(win_height-4, self.tracks_win_width, 2, int(win_width*0.3))
 
 		self.playlists_window.border('|', '|', '-', '-', '+', '+', '+', '+')
 		self.tracks_window.border('|', '|', '-', '-', '+', '+', '+', '+')
@@ -39,15 +46,28 @@ class MainWorker:
 		self.update_playlists_list()
 		curses.doupdate()
 
+
 	def update_playlists_list(self):
 		self.playlists = self.db_worker.get_playlists()
-		for i, playlist_id in enumerate(sorted(list(self.playlists.keys()))[self.first_displayed_playlist:self.first_displayed_playlist+self.global_lists_height]):
-			self.playlists_window.addstr(1+i, 1, "[ ] "+self.playlists[playlist_id])
+		playlist_ids = sorted(list(self.playlists.keys()))
+		self.playlists_count = len(playlist_ids)
+
+		if(self.selected_playlist == None):
+			self.selected_playlist = 0
+
+		for i, playlist_id in enumerate(playlist_ids[self.first_displayed_playlist:self.first_displayed_playlist+self.global_lists_height]):
+			s = "["+("*" if (i+self.first_displayed_playlist)==self.selected_playlist else " ")+"] "+self.playlists[playlist_id]
+			self.playlists_window.addstr(1+i, 1, s+(" "*(self.playlists_win_width-2-len(s))))
+
+		for i in range(i+1, self.global_lists_height):
+			self.playlists_window.addstr(1+i, 1, " "*(self.playlists_win_width-2))
 		self.playlists_window.refresh()
+
 
 	def update_player(self):
 		self.player_heading_window.addstr(0, 0, 'test')
-		self.player_heading_window.noutrefresh()
+		self.player_heading_window.refresh()
+
 
 	#values from 0 to 1000!!
 	def set_player_progressbar(self, value):
@@ -55,6 +75,7 @@ class MainWorker:
 		s = "="*round(l)+"-"*(self.global_width-round(l))
 		self.player_progressbar_window.addstr(0, 0, s)
 		self.player_progressbar_window.refresh()
+
 
 	def update_player_heading(self):
 		data = self.db_worker.get_track_data(self.playback_id)
@@ -66,6 +87,72 @@ class MainWorker:
 		self.player_heading_window.addstr(0, 0, s)
 		self.player_heading_window.refresh()
 
+
+	def do_import(self):
+		win_height = int(curses.LINES*0.5)
+		win_width  = int(curses.COLS*0.5)
+		import_window = curses.newwin(win_height, win_width, int((curses.LINES-win_height)/2), int((curses.COLS-win_width)/2))
+		import_window.keypad(True)
+		import_window.border('|', '|', '-', '-', '+', '+', '+', '+')
+		import_window.addstr(1, 1, "Use arrows for navigation")
+		import_window.addstr(2, 1, "Use Enter to choose")
+		import_window.addstr(4, 1, "Pick file to import:")
+		import_window.addstr(5, 1, "-"*(win_width-2))
+		#small file explorer here
+		current_path = os.getcwd()
+		chosen_file = None
+		while(True):
+			path_list = os.listdir(current_path)
+			dir_list = []
+			files_list = []
+			for path in path_list:
+				if(os.path.isdir(path)): dir_list.append(path)
+				else: files_list.append(path)
+			path_list = []
+			path_list.append("..")
+			dir_list.sort()
+			files_list.sort()
+			path_list += dir_list+files_list
+			first_displayed = 0
+			selected = 0
+			while(chosen_file == None):
+				for i,file in enumerate(path_list[first_displayed:first_displayed+(win_height-6)]):
+					import_window.addstr(i+6, 1, ("[*] " if selected==first_displayed+i else "[ ] ")+file+" "*(win_width-6-len(file)))
+				for i in range(i+1, win_height-7):
+					import_window.addstr(i+6, 1, " "*(win_width-2))
+				c = import_window.getch()
+				if(c == curses.KEY_UP and selected > 0):
+					selected -= 1
+					if(selected-first_displayed < 3 and first_displayed != 0): first_displayed -= 1
+				if(c == curses.KEY_DOWN and selected < len(path_list)-1):
+					selected += 1
+					if(win_height-6+first_displayed-selected < 3): first_displayed += 1
+				if(c == ord('\n')):
+					if(os.path.isdir(path_list[selected])): current_path = os.path.join(current_path, path_list[selected])
+					else: chosen_file = os.path.join(current_path, path_list[selected])
+					break
+		import_window.clear()
+		
+
+
+	def mainloop(self):
+		while(True):
+			c = self.playlists_window.getch()
+			if(c == ord('w') and self.selected_playlist > 0): #playlist up
+				self.selected_playlist -= 1
+				if(self.selected_playlist-self.first_displayed_playlist < 5 and self.first_displayed_playlist != 0): self.first_displayed_playlist -= 1
+				self.update_playlists_list()
+			if(c == ord('s') and self.selected_playlist < self.playlists_count-1): #playlist down
+				self.selected_playlist += 1
+				if(self.global_lists_height+self.first_displayed_playlist-self.selected_playlist < 5): self.first_displayed_playlist += 1
+				self.update_playlists_list()
+
+			if(c == ord('i')): #import
+				self.do_import()
+			if(c == ord('q')):
+				break
+
+
 def main(screen):
 	db_import_worker = DBImportWorker.DBImportWorker("vimusic.db")
 	db_worker = DBWorker.DBWorker()
@@ -74,10 +161,10 @@ def main(screen):
 
 	main_worker = MainWorker(screen, yt_worker, db_worker)
 	main_worker.update_player_heading()
-	for i in range(1000):
-		main_worker.set_player_progressbar(i)
-		time.sleep(0.1)
-	input()
+	main_worker.mainloop()
+	#for i in range(1000):
+	#	main_worker.set_player_progressbar(i)
+	#	time.sleep(0.1)
 	#print(playlists)
 	#print(yt_worker.get_info("pTYIf2pkxzQ"))
 	#yt_worker.get_audio("pTYIf2pkxzQ")
